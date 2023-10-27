d840a99b670d41eec9a228790551ae2d8f627dd6
artyom <batmanov.2016@mail.ru>
2023-10-27 22:30:53 +0300

    urls changed, improved SignUpView

--- megano/app_users/views.py
+++ megano/app_users/views.py
@@ -22,6 +22,8 @@ class SignUpView(APIView):
 
         try:
             user = User.objects.create_user(username=username, password=password)
+            user.first_name = name
+            user.save()
             profile = Profile.objects.create(user=user, first_name=name)
             user = authenticate(request, username=username, password=password)
             if user is not None:
