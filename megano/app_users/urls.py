17b8303e2464517ddaf7796464d36472bd361ee9
Artyom Batmanov <batmanov.2016@mail.ru>
2023-10-27 13:47:22 +0300

    change urls in app_users

--- megano/app_users/urls.py
+++ megano/app_users/urls.py
@@ -4,11 +4,11 @@ from django.urls import path, include
 from .views import AuthView, LogoutView, SignUpView, ProfileView, AvatarUpdateView, ChangePasswordView
 
 urlpatterns = [
-    path('api/sign-up/', SignUpView.as_view(), name='sign-up'),
-    path('api/sign-out/', LogoutView.as_view(), name='sign-out'),
-    path('api/sign-in/', AuthView.as_view(), name='login'),
-    path('api/profile/', ProfileView.as_view(), name='profile'),
-    path('api/profile/avatar/', AvatarUpdateView.as_view(), name='avatar'),
-    path('api/profile/password/', ChangePasswordView.as_view(), name='password'),
+    path('api/sign-up', SignUpView.as_view(), name='sign-up'),
+    path('api/sign-out', LogoutView.as_view(), name='sign-out'),
+    path('api/sign-in', AuthView.as_view(), name='login'),
+    path('api/profile', ProfileView.as_view(), name='profile'),
+    path('api/profile/avatar', AvatarUpdateView.as_view(), name='avatar'),
+    path('api/profile/password', ChangePasswordView.as_view(), name='password'),
 
 ]
